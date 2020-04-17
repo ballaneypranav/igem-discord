7ee12a67b3d1d08d974a6a3bd5a218edec4708f6
Pranav Ballaney <ballaneypranav@gmail.com>
2020-04-17 18:56:21 +0530

    Join/remove member greeting

--- bot.py
+++ bot.py
@@ -8,4 +8,15 @@ client = commands.Bot(command_prefix=".")
 async def on_ready():
     print('Bot is ready.')
 
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+
+
 client.run(os.getenv('DISCORD_TOKEN'))
